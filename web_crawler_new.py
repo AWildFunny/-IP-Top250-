2f088f74062baad0b4d68a926159f7ff9149da89
胡恒嘉 <54237951+AWildFunny@users.noreply.github.com>
2024-11-15 22:42:36 +0800

    Update web_crawler_new.py

--- web_crawler_new.py
+++ web_crawler_new.py
@@ -1,13 +1,19 @@
+# 网络爬虫
+# import pandas as pd
+# df=pd.DataFrame({'b':[1,2,3,3,4],'c':[22,33,22,44,66]},index=[10,20,30,40,50])
+#
+# a=df[(df.b==3)&(df.c==22)]
 # -*- codeing = utf-8 -*-
-from bs4 import BeautifulSoup
-import re
-import urllib.request, urllib.error
-import xlwt
+from bs4 import BeautifulSoup  # 网页解析，获取数据
+import re  # 正则表达式，进行文字匹配`
+import urllib.request, urllib.error  # 制定URL，获取网页数据
+import xlwt  # 进行excel操作
 import random  # 添加用于随机选择代理IP
 import time  # 添加用于延时
-
-# 保持原有的正则表达式定义不变
-findLink = re.compile(r'<a href="(.*?)">')
+#import sqlite3  # 进行SQLite数据库操作
+#一些find开头的是正则表达式，是用来我们筛选信息
+#https://blog.csdn.net/weixin_52122271/article/details/118337446
+findLink = re.compile(r'<a href="(.*?)">')  # 创建正则表达式对象，标售规则影片详情链接的规则
 findImgSrc = re.compile(r'<img.*src="(.*?)"', re.S)
 findTitle = re.compile(r'<span class="title">(.*)</span>')
 findRating = re.compile(r'<span class="rating_num" property="v:average">(.*)</span>')
@@ -15,6 +21,7 @@ findJudge = re.compile(r'<span>(\d*)人评价</span>')
 findInq = re.compile(r'<span class="inq">(.*)</span>')
 findBd = re.compile(r'<p class="">(.*?)</p>', re.S)
 
+# 添加代理服务器配置
 proxyHost = "dyn.horocn.com"
 proxyPort = "50000"
 proxyUser = "B8FG1815792430398662"  # 替换为你的订单号
@@ -27,71 +34,90 @@ proxyMeta = "http://%(user)s:%(pass)s@%(host)s:%(port)s" % {
     "pass": proxyPass,
 }
 
-
 def main():
-    baseurl = "https://movie.douban.com/top250?start="
+    baseurl = "https://movie.douban.com/top250?start="  #要爬取的网页链接
+    # 1.爬取网页，baseurl是要爬虫的网址，调用了getData（baseurl)
     datalist = getData(baseurl)
-    savepath = "./豆瓣电影Top250.xls"
-    saveData(datalist, savepath)
-
-
+    savepath = "豆瓣电影Top250.xls"    #当前目录新建XLS，存储进去
+    # dbpath = "movie.db"              #当前目录新建数据库，存储进去
+    # 3.保存数据
+    saveData(datalist,savepath)      #2种存储方式可以只选择一种
+    # saveData2DB(datalist,dbpath)
+#
+# 爬取网页
 def getData(baseurl):
-    datalist = []
-    for i in range(0, 10):
+    datalist = []  #用来存储爬取的网页信息
+    for i in range(0, 10):  # 调用获取页面信息的函数，10次
+        #因为电影评分Top250，每个页面只显示25个，需要访问页面10次
         url = baseurl + str(i * 25)
+        #比如i=1时, https://movie.douban.com/top250?start=25
         print(f"正在爬取第 {i + 1} 页...")
-        # 添加随机延时
-        time.sleep(random.uniform(1, 3))
-        html = askURL(url)
-        if not html:
-            print(f"第 {i + 1} 页爬取失败，尝试下一页...")
-            continue
 
-        soup = BeautifulSoup(html, "html.parser")
-        for item in soup.find_all('div', class_="item"):
+        # 添加重试机制
+        max_attempts = 3  # 最大重试次数
+        current_attempt = 0
+        html = None
+
+        while current_attempt < max_attempts:
+            current_attempt += 1
             try:
-                data = []
-                item = str(item)
-                link = re.findall(findLink, item)[0]
-                data.append(link)
-                imgSrc = re.findall(findImgSrc, item)[0]
-                data.append(imgSrc)
-                titles = re.findall(findTitle, item)
-                if (len(titles) == 2):
-                    ctitle = titles[0]
-                    data.append(ctitle)
-                    otitle = titles[1].replace("/", "")
-                    data.append(otitle)
+                # 添加随机延时
+                time.sleep(random.uniform(1, 3))
+                html = askURL(url)
+                if html:  # 如果成功获取页面
+                    print(f"第 {i + 1} 页爬取成功！")
+                    break
                 else:
-                    data.append(titles[0])
-                    data.append(' ')
-                rating = re.findall(findRating, item)[0]
-                data.append(rating)
-                judgeNum = re.findall(findJudge, item)[0]
-                data.append(judgeNum)
-                inq = re.findall(findInq, item)
-                if len(inq) != 0:
-                    inq = inq[0].replace("。", "")
-                    data.append(inq)
-                else:
-                    data.append(" ")
-                bd = re.findall(findBd, item)[0]
-                bd = re.sub('<br(\s+)?/>(\s+)?', "", bd)
-                bd = re.sub('/', "", bd)
-                data.append(bd.strip())
-                datalist.append(data)
+                    print(f"第 {i + 1} 页第 {current_attempt} 次尝试失败")
             except Exception as e:
-                print(f"解析数据时出错: {str(e)}")
-                continue
+                print(f"第 {i + 1} 页第 {current_attempt} 次尝试出错: {str(e)}")
+
+        if not html:
+            print(f"第 {i + 1} 页在 {max_attempts} 次尝试后仍然失败，跳过该页...")
+            continue
+            
+        # 2.逐一解析数据
+        soup = BeautifulSoup(html, "html.parser")
+        for item in soup.find_all('div', class_="item"):  # 查找符合要求的字符串
+            data = []  # 保存一部电影所有信息
+            item = str(item)
+            link = re.findall(findLink, item)[0]  # 通过正则表达式查找
+            data.append(link)
+            imgSrc = re.findall(findImgSrc, item)[0]
+            data.append(imgSrc)
+            titles = re.findall(findTitle, item)
+            if (len(titles) == 2):
+                ctitle = titles[0]
+                data.append(ctitle)
+                otitle = titles[1].replace("/", "")  #消除转义字符
+                data.append(otitle)
+            else:
+                data.append(titles[0])
+                data.append(' ')
+            rating = re.findall(findRating, item)[0]
+            data.append(rating)
+            judgeNum = re.findall(findJudge, item)[0]
+            data.append(judgeNum)
+            inq = re.findall(findInq, item)
+            if len(inq) != 0:
+                inq = inq[0].replace("。", "")
+                data.append(inq)
+            else:
+                data.append(" ")
+            bd = re.findall(findBd, item)[0]
+            bd = re.sub('<br(\s+)?/>(\s+)?', "", bd)
+            bd = re.sub('/', "", bd)
+            data.append(bd.strip())
+            datalist.append(data)
 
     return datalist
-
-
+#
+# 得到指定一个URL的网页内容
 def askURL(url):
     head = {  # 模拟浏览器头部信息，向豆瓣服务器发送消息
         "User-Agent": "Mozilla / 5.0(Windows NT 10.0; Win64; x64) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 80.0.3987.122  Safari / 537.36"
     }
-
+    # 用户代理，表示告诉豆瓣服务器，我们是什么类型的机器、浏览器（本质上是告诉浏览器，我们可以接收什么水平的文件内容）
     try:
         # 设置代理
         proxy_handler = urllib.request.ProxyHandler({
@@ -110,33 +136,114 @@ def askURL(url):
     except Exception as e:
         print(f"访问URL失败: {str(e)}")
         return None
+#
+# 保存数据到表格
+def saveData(datalist,savepath):
+    print("save.......")
+    book = xlwt.Workbook(encoding="utf-8",style_compression=0) #创建workbook对象
+    sheet = book.add_sheet('豆瓣电影Top250', cell_overwrite_ok=True) #创建工作表
+    col = ("电影详情链接","图片链接","影片中文名","影片外国名","评分","评价数","概况","相关信息")
+    for i in range(0,8):
+        sheet.write(0,i,col[i])  #列名
+    for i in range(0,250):
+        # print("第%d条" %(i+1))       #输出语句，用来测试
+        data = datalist[i]
+        for j in range(0,8):
+            sheet.write(i+1,j,data[j])  #数据
+    book.save(savepath) #保存
+
+# def saveData2DB(datalist,dbpath):
+#     init_db(dbpath)
+#     conn = sqlite3.connect(dbpath)
+#     cur = conn.cursor()
+#     for data in datalist:
+#             for index in range(len(data)):
+#                 if index == 4 or index == 5:
+#                     continue
+#                 data[index] = '"'+data[index]+'"'
+#             sql = '''
+#                     insert into movie250(
+#                     info_link,pic_link,cname,ename,score,rated,instroduction,info)
+#                     values (%s)'''%",".join(data)
+#             # print(sql)     #输出查询语句，用来测试
+#             cur.execute(sql)
+#             conn.commit()
+#     cur.close
+#     conn.close()
 
 
-def saveData(datalist, savepath):
-    print("开始保存数据...")
-    try:
-        book = xlwt.Workbook(encoding="utf-8", style_compression=0)
-        sheet = book.add_sheet('豆瓣电影Top250', cell_overwrite_ok=True)
-        col = ("电影详情链接", "图片链接", "影片中文名", "影片外国名", "评分", "评价数", "概况", "相关信息")
+# def init_db(dbpath):
+#     sql = '''
+#         create table movie250(
+#         id integer  primary  key autoincrement,
+#         info_link text,
+#         pic_link text,
+#         cname varchar,
+#         ename varchar ,
+#         score numeric,
+#         rated numeric,
+#         instroduction text,
+#         info text
+#         )
+#
+#
+#     '''  #创建数据表
+#     conn = sqlite3.connect(dbpath)
+#     cursor = conn.cursor()
+#     cursor.execute(sql)
+#     conn.commit()
+#     conn.close()
 
-        # 写入列名
-        for i in range(0, 8):
-            sheet.write(0, i, col[i])
+# 保存数据到数据库
 
-        # 写入数据
-        for i in range(len(datalist)):
-            print(f"正在保存第 {i + 1} 条数据")
-            data = datalist[i]
-            for j in range(0, 8):
-                sheet.write(i + 1, j, data[j])
-
-        book.save(savepath)
-        print(f"数据保存成功！共保存 {len(datalist)} 条记录到 {savepath}")
-    except Exception as e:
-        print(f"保存数据时出错: {str(e)}")
+if __name__ == "__main__":  # 当程序执行时
+    # 调用函数
+     main()
+    # init_db("movietest.db")
+     print("爬取完毕！")
 
 
-if __name__ == "__main__":
-    print("开始爬取...")
-    main()
-    print("爬取完毕！")
+
+# #Json文件
+# import pandas as pd
+# import json
+# import datetime
+# #数招来源网https://openweathermap.org/current
+# file=open('weather.json','r')
+# text=file.read()
+# text=json.loads(text)
+# # print(text)
+# # print(list(text.keys()))
+#
+# def pre(text,city) :
+#     temp=[]
+#     humidity=[]
+#     pressure=[]
+#     description=[]
+#     dt=[]
+#     wind_speed=[]
+#     wind_deg=[]
+#     sea_level=[]
+#     grnd_level=[]
+#
+#     temp.append(text['main']['temp']-273.15)
+#     humidity.append(text['main']['humidity'])
+#     pressure.append(text['main']['pressure'])
+#     description.append(text['weather'][0]['description'])
+#     dt.append(text['dt'])
+#     wind_speed.append(text['wind']['speed'])
+#     wind_deg.append(text['wind']['deg'])
+#     sea_level.append(text['main']['sea_level'])
+#     grnd_level.append(text['main']['grnd_level'])
+#
+#     headings=['temp','humidity','pressure','description','dt','wind_speed','wind_deg','sea_level','grnd_level']
+#     data=[temp,humidity,pressure,description,dt,wind_speed,wind_deg, sea_level, grnd_level]
+#     df=pd.DataFrame(data, index=headings)
+#     text=df.T #阵转置
+#     text['city']=city #新增city列/#新增day列，将时间歌转换为常用日期时间格式
+#     text['day']=text['dt'].apply(datetime.datetime.fromtimestamp)
+#     text.drop(columns='dt',inplace=True)  # 删除多会的dt列/
+#     return text
+#
+# df=pre(text, text['name'])
+# print(df)
